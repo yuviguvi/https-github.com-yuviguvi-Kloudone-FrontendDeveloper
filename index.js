f8fa4a9a410007b5cc463638af6382cad152c4d6
yuviguvi <yuvashreemahendran@gmail.com>
2020-06-04 14:31:01 +0530

    2 files modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+/*// Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter</h1>`;*/
